3c62ca7dd73a5f54c40b882eef8773dfab7c1915
christineronan <ronandesign1@Station-1--Christine-Ronan.local>
2014-07-15 16:24:24 -0500

    new jquery lightbox attempt Round 4 071514

---
+++ __Round 4/js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$("#wizards a").lightBox();
+});
+$(document).ready(function() {
+	$("a[href$=.jpg]").lightBox(); 
+	$("a[href$=.gif]").lightBox();
+	$("a[href$=.png]").lightBox();
+});
